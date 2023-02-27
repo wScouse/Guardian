0cb48c80b355d2cd89902698395e439a487b41fa
Artemis <williamscouse@outlook.com>
2023-02-27 22:14:39 +0000

    Home Page Design

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark d-flex flex-column justify-content-center align-items-center vh-100">
+      <div className="bg-black-50 text-white w-100 position-fixed d-flex justify-content-center align-items-center">
+        <h1 className="mb-0 text-white px-3 py-2" style={{fontSize: '4rem'}}>Guardian</h1>
+      </div>
+      <div className="mt-5 pt-5">
+        <button type="button" className="btn btn-primary mx-3">Report</button>
+        <button type="button" className="btn btn-secondary mx-3">Login</button>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
