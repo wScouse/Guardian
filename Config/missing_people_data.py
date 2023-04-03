fce79befcd47419f68c2df16115e954413abdc51
Will Scouse <williamscouse@outlook.com>
2023-04-03 16:39:26 +0100

    DB Data Insert

--- Config/missing_people_data.py
+++ Config/missing_people_data.py
@@ -1,12 +1,73 @@
 # Create MySQL table to store missing people data
 
 import mysql.connector
+import datetime
 
-# conn_object=mysql.connector.connect(
-#     host="localhost", user="guardian",
-#     password="", database="guardian_missing_people_data")
-print("Connecting to..")
+print("Connecting to DB..")
+missingDB=mysql.connector.connect(
+    host="localhost", user="Guardian",
+    password="", database="guardian_missing_people_data")
+
 #WSL
-conn_object=mysql.connector.connect(host="172.21.192.1", user="wsl_root", password="", database="guardian_missing_people_data")
+# conn_object=mysql.connector.connect(host="172.21.192.1", user="wsl_root", password="", database="guardian_missing_people_data")
 
 print("Test")
+print(missingDB)
+
+# Data Inputs
+
+## Missing Person Data
+print("\n")
+print("Missing Person Data")
+dataName = input("Name: ")
+dataAge = input("Age: ")
+dataGender = input("Gender: ")
+dataLocation = input("Last known location: ")
+dataInfo = input("Additional Info: ")
+dataDate = datetime.datetime.now()
+print("\n")
+
+# Reporting Person Contact
+print("Contact Details")
+dataReporting = input("Name: ")
+dataMobile = input("Mobile: ")
+dataEmail = input("Email: ")
+
+
+# Get a cursor object
+cursor = missingDB.cursor()
+
+# Start the transaction
+missingDB.start_transaction
+
+try:
+
+    kinData = (dataReporting, dataMobile, dataEmail)
+    cursor.execute("INSERT INTO kin_data (kinNAME, kinMOBILE, kinEMAIL) VALUES (%s, %s, %s)", kinData)
+
+    # Get the KinID of the inserted row
+    kinID = cursor.lastrowid
+
+    # Add data to database.
+    sql = "INSERT INTO missing_people_data (missingNAME, missingAGE, missingGENDER, missingLOCATION, missingINFO, missingFROM, kinID) values (%s, %s, %s, %s, %s, %s, %s)"
+    val = (dataName, dataAge, dataGender, dataLocation, dataInfo, dataDate, kinID)
+    cursor.execute(sql, val)
+
+    # Commit transaction
+    missingDB.commit()
+
+    print("Data inserted!")
+
+except Exception as e:
+    # Rollback the transaction if an error occurred.
+    missingDB.rollback()
+    print("Error: ", e)
+
+# display all records
+# table = cursor.fetchall()
+
+# # Close cursor
+# cursor.close()
+
+# # Close connection object
+# missingDB.close()
