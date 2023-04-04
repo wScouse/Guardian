c653d11655d9f336cad8fd74173b9ba4eaa9f5ed
Will Scouse <williamscouse@outlook.com>
2023-04-04 19:21:04 +0100

    Match Data Extraction

--- Face_Recognition/ImageSearch.py
+++ Face_Recognition/ImageSearch.py
@@ -6,8 +6,9 @@ from tkinter import filedialog
 from deepface import DeepFace
 import cv2
 import matplotlib.pyplot as plt
-from PIL import Image
-import pandas as pd
+# from PIL import Image
+# import pandas as pd
+import mysql.connector
 
 # Tkinter settings
 root = tk.Tk()
@@ -51,8 +52,8 @@ def findImage(img1):
             # Get the ID
             missingID = missingFile.split(".")[0]
 
-            print("Alert! Missing person identified! ID:", missingID)
-            print("\n")
+            # print("Alert! Missing person identified! ID:", missingID)
+            # print("\n")
 
             matchingIDs.append(missingID)
 
@@ -61,15 +62,65 @@ def findImage(img1):
         except IndexError:
             break
 
+    
     return matchingIDs
+    
+    
 
 
 def extractData(caseIDs):
     # This function will extract the data for matching IDs.
+
+    guardianDB=mysql.connector.connect(
+        host="localhost", user="Guardian",
+        password="", database="guardian_missing_people_data")
+    cursor = guardianDB.cursor()  
+
+    for x in caseIDs:
+
+        # Get details by missingID
+        sql = "SELECT * FROM missing_people_data WHERE photoID = %s"
+        cursor.execute(sql, (x,))
+
+        caseInfo = cursor.fetchone()
+
+
+        if caseInfo == None:
+            print("No match for given ID: %s" % x)
+        else:
+            # Extracts data and assigns them to variables
+            caseID, caseName, caseFrom, caseLocation, caseAge, caseGender, caseAinfo, caseReporter, casePhoto, caseCapture, caseEthreat, caseAthreat, caseStatus, caseEnd = caseInfo
+
+            print("\nID: %s matches Case ID: %s" % (x, caseID))
+            print("\n")
+            print("Name: %s" % caseName)
+            print("Age: %s" % caseAge)
+            print("Gender: %s" % caseGender)
+            print("Reported Missing: %s" % caseFrom)
+            print("Last known location: %s" % caseLocation)
+            print("Additional Info: %s" % caseAinfo)
+
+            print("Estimated Threat: %s" % caseEthreat)
+            print("Actual Threat")
+
+            print("Reported by %s" % caseReporter)
+            print("Reported on: ")
+            print("\n")
+
+
+
     
+    cursor.close()
+    guardianDB.close()
 
 
 img1 = fileSelector()
 matchingIDs = findImage(img1)
 
-print(matchingIDs)
+if not matchingIDs:
+    # If there are no matches this message is displayed.
+    print("No matches found.")
+else:
+    # Otherwise the details are extracted.
+    print("The following Case IDs have been found: %s \n" % matchingIDs)
+    extractData(matchingIDs)
