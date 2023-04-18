3030b5c03f95049743390ebd18563caf7129bc98
Will Scouse <williamscouse@outlook.com>
2023-04-18 19:13:13 +0100

    Direct to Request Page

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Request from './pages/Request';
 import Report from './pages/Report';
 import Guardian from './pages/Guardian';
 import Detections from './pages/Detections';
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         < Route path="/" exact element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/request" element={<Request />} />
         <Route path="/report" element={<Report />} />
         <Route path="/guardian" element={<Guardian />} />
         <Route path="/detections" element={<Detections />} />
--- front/src/pages/Request.js
+++ front/src/pages/Request.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from'react-router-dom';
+import axios from 'axios';
 
 
-function Login(){
+function Request(){
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
-    const data = { email: email, password: password};
+    const data = { email: email};
     fetch('http://localhost:5000/api/login', {
       method: 'POST',
       headers: {
@@ -29,22 +29,18 @@ function Login(){
         <Link to="/" className="text-decoration-none">
           <h1 className="mb-0 text-white px-3 py-2 mt-5" style={{fontSize: '4rem'}}>Guardian</h1>
         </Link>
-        <h2 className="text-white mb-4">Login</h2>
+        <h2 className="text-white mb-4">Request Access</h2>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="emailInput" className="text-white">Email address</label>
             <input type="email" className="form-control" id="emailInput" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
           </div>
-          <div className="form-group">
-            <label htmlFor="passwordInput" className="text-white">Password</label>
-            <input type="password" className="form-control" id="passwordInput" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-          </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
-        <Link className="btn btn-primary" to="/request">Request Access</Link>
       </div>
     </div>
   );
 }
 
-export default Login;
+
+export default Request;
